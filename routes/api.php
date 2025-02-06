4564d42ef9b710288bdc1492511d1903ecf2bb96
Hassan-Shahriar-1 <hassanshahriar18@gmail.com>
2025-02-06 15:09:25 +0600

    rename product list

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'], function () {
-    Route::get('product-details', [ProductController::class, 'allProducts']);
+    Route::get('product-list', [ProductController::class, 'allProducts']);
 });
